e3ae8c46c4d75e9b5aa78f660c6572533e29a27f
rushba <rushba@corp.badoo.com>
2021-08-19 21:00:57 +0300

    fix

--- app.py
+++ app.py
@@ -2,24 +2,20 @@
 from datetime import timedelta
 
 from flask import Flask
-from flask_restful import Api
 from flask_jwt import JWT
+from flask_restful import Api
 
-from security import authenticate, identity
-from resources.item import Item, ItemList
-from resources.user import UserRegister
-from resources.store import Store, StoreList
 from db import db
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from resources.user import UserRegister
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'jopa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 jwt = JWT(app, authenticate, identity)
